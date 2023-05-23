0b3e5fae3fad7df65c00a162b3df1b22506b2f59
AnmZ <anmit.ray911@gmail.com>
2023-05-23 08:13:02 +0530

    Added random seed generators

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,14 +3,25 @@ package db
 import (
 	"context"
 	"testing"
+
+	"github.com/anmit007/BankBackend/util"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "Anmit",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 
 }
